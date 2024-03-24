e2dd632be8952c51f182e886c4d24a1fa71b5cae
jochongs <joch2712@naver.com>
2024-03-24 12:42:44 +0900

    first commit

---
+++ src/test/java/practice2/core/SpringPractice2ApplicationTests.java
@@ -0,0 +1,13 @@
+package practice2.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPractice2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
