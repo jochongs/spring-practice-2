aa09aa62ba2620d9e14dfd3ecf31337448c27574
jochongs <joch2712@naver.com>
2024-03-25 20:44:59 +0900

    feat: apply spring DI container to OrderApp

--- src/main/java/practice2/core/OrderApp.java
+++ src/main/java/practice2/core/OrderApp.java
@@ -1,5 +1,7 @@
 package practice2.core;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import practice2.core.member.Grade;
 import practice2.core.member.Member;
 import practice2.core.member.MemberService;
@@ -11,9 +13,14 @@ import practice2.core.order.OrderServiceImpl;
 public class OrderApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
+
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        OrderService orderService = ac.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId, "Jochong", Grade.VIP);
