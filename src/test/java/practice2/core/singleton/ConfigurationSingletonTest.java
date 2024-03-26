2472a85d5458c97363f665248991122fd1b199ef
jochongs <joch2712@naver.com>
2024-03-26 17:33:45 +0900

    feat: modify AppConfig

--- src/test/java/practice2/core/singleton/ConfigurationSingletonTest.java
+++ src/test/java/practice2/core/singleton/ConfigurationSingletonTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import practice2.core.AppConfig;
+import practice2.core.member.MemberRepository;
 import practice2.core.member.MemberServiceImpl;
 import practice2.core.order.OrderServiceImpl;
 
@@ -16,10 +17,14 @@ public class ConfigurationSingletonTest {
 
         MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
         OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
+        MemberRepository memberRepository = ac.getBean("memberRepository", MemberRepository.class);
 
         Assertions.assertThat(memberService.getMemberRepository()).isSameAs(orderService.getMemberRepository());
+        Assertions.assertThat(memberRepository).isSameAs(orderService.getMemberRepository());
+        Assertions.assertThat(memberRepository).isSameAs(memberService.getMemberRepository());
 
         System.out.println("memberService.getMemberRepository() = " + memberService.getMemberRepository());
         System.out.println("orderService.getMemberRepository() = " + orderService.getMemberRepository());
+        System.out.println("memberRepository = " + memberRepository);
     }
 }
