7846a7acecb4361e3d1e1994618ade5cf7e83c76
jochongs <joch2712@naver.com>
2024-03-26 13:29:26 +0900

    feat: add ApplicationContextBasicFindTest

---
+++ src/test/java/practice2/core/beanfind/ApplicationContextBasicFindTest.java
@@ -0,0 +1,41 @@
+package practice2.core.beanfind;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import practice2.core.AppConfig;
+import practice2.core.member.MemberService;
+import practice2.core.member.MemberServiceImpl;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class ApplicationContextBasicFindTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("Find bean by name of bean")
+    void findBeanByName() {
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+
+        System.out.println("memberService = " + memberService);
+        System.out.println("memberService = " + memberService.getClass());
+
+        assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+
+    @Test
+    @DisplayName("Find bean by abstract class type")
+    void findBeanByAbstractClassType() {
+        MemberService memberService = ac.getBean(MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("Find bean by concrete class type")
+    void findBeanByConcreteClassType() {
+        MemberServiceImpl memberService = ac.getBean(MemberServiceImpl.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+}
