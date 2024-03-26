dab0595b50818db1d5a7fc2ea239cda8b9925dc7
jochongs <joch2712@naver.com>
2024-03-26 13:33:26 +0900

    feat: cannot find bean exception test case

--- src/test/java/practice2/core/beanfind/ApplicationContextBasicFindTest.java
+++ src/test/java/practice2/core/beanfind/ApplicationContextBasicFindTest.java
@@ -3,12 +3,14 @@ package practice2.core.beanfind;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import practice2.core.AppConfig;
 import practice2.core.member.MemberService;
 import practice2.core.member.MemberServiceImpl;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ApplicationContextBasicFindTest {
 
@@ -37,5 +39,14 @@ public class ApplicationContextBasicFindTest {
     void findBeanByConcreteClassType() {
         MemberServiceImpl memberService = ac.getBean(MemberServiceImpl.class);
         assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("Find bean by name but cannot find bean")
+    void findBeanByNameButNoBean() {
+        assertThrows(NoSuchBeanDefinitionException.class,
+                () -> {
+                    ac.getBean("xxxx", MemberService.class);
+                });
     }
 }
