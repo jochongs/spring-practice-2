7da7c807a522ae2ac7be9df534573638c7bd5ba0
jochongs <joch2712@naver.com>
2024-03-26 15:09:56 +0900

    chore: create appConfix.xml

---
+++ src/test/java/practice2/core/xml/XmlAppContext.java
@@ -0,0 +1,16 @@
+package practice2.core.xml;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.support.GenericXmlApplicationContext;
+import practice2.core.member.MemberService;
+
+public class XmlAppContext {
+
+    @Test
+    void xmlAppContext() {
+        GenericXmlApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
