06a4164ad3ccc390579afb018c06d8dd39d67925
jochongs <joch2712@naver.com>
2024-03-26 17:22:32 +0900

    test: compare memberRepository instance address

---
+++ src/test/java/practice2/core/singleton/ConfigurationSingletonTest.java
@@ -0,0 +1,22 @@
+package practice2.core.singleton;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import practice2.core.AppConfig;
+import practice2.core.member.MemberServiceImpl;
+import practice2.core.order.OrderServiceImpl;
+
+public class ConfigurationSingletonTest {
+
+    @Test
+    void configurationTest() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
+
+        Assertions.assertThat(memberService.getMemberRepository()).isSameAs(orderService.getMemberRepository());
+    }
+}
